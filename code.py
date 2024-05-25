3097419e638c0a7dc14731c3c0f061cb37b08e8b
iit2018062 <iit2018062@iiita.ac.in>
2024-05-25 14:53:40 -0700

    added data

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ import csv
 import pandas as pd
 import requests
 from datetime import datetime
+import json
 
 def get_vehicle_ids_from_csv(filename, column_name):
     df = pd.read_csv(filename)
@@ -21,19 +22,12 @@ def save_to_file(vehicle_id, content):
     directory = f'Data/{today_date}'
     if not os.path.exists(directory):
         os.makedirs(directory)
-    filename = os.path.join(directory, f"{today_date}.csv")  # Use today's date for the filename
+    filename = os.path.join(directory, f"{vehicle_id}.json")  # Use vehicle ID for the filename
 
-    # Write data to CSV file if status code is 200
+    # Write data to JSON file if status code is 200
     if content and status_code == 200:
-        with open(filename, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-
-            # Write header
-            writer.writerow(content[0].keys())
-
-            # Write rows
-            for item in content:
-                writer.writerow(item.values())
+        with open(filename, 'w') as jsonfile:
+            json.dump(content, jsonfile, indent=4)
         print(f"Data for Vehicle ID {vehicle_id} saved to file: {filename}")
     else:
         print(f"Data for Vehicle ID {vehicle_id} not saved. Status code: {status_code}")
